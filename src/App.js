84f0167a2c738badee5c1e109efad8446ee9862f
Ali Raza <alisabri022@gmail.com>
2021-07-31 13:19:00 +0500

    build: Typography theme is build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider, Typography } from "@material-ui/core";
 
+import theme from "./Theme";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <Typography variant="h1">hello world</Typography>
+      </ThemeProvider>
     </div>
   );
 }
---
+++ src/Theme.js
@@ -0,0 +1,58 @@
+import { createTheme } from "@material-ui/core";
+const theme = createTheme({
+  props: {
+    MuiTypography: {
+      variantMapping: {
+        h1: "h1",
+        h2: "h2",
+        h3: "h3",
+        h4: "h4",
+        h5: "h5",
+        h6: "h6",
+        subtitle1: "h2",
+        subtitle2: "h2",
+        body1: "span",
+        body2: "span",
+      },
+    },
+  },
+  typography: {
+    h1: {
+      fontSize: "3rem",
+      fontWeight: "300",
+    },
+    h2: {
+      fontSize: "2.5rem",
+      fontWeight: "300",
+    },
+    h3: {
+      fontSize: "2rem",
+      fontWeight: "300",
+    },
+    h4: {
+      fontSize: "1.5rem",
+      fontWeight: "300",
+    },
+    h5: {
+      fontSize: "1.25rem",
+      fontWeight: "300",
+    },
+    h6: {
+      fontSize: "1rem",
+      fontWeight: "300",
+    },
+  },
+
+  overrides: {
+    MuiCssBaseline: {
+      "@global": {
+        ".link": {
+          textDecoration: "none",
+          color: "white",
+        },
+      },
+    },
+  },
+});
+
+export default theme;
